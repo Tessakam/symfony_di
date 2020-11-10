91bd8dd95eca573719da37ae715fd186ef0129a9
Tessakam <tessa.kam@hotmail.com>
2020-11-10 11:17:00 +0100

    dashes are working!

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\transform;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Capitalize;
+use App\Entity\SpacesToDashes;
 
 class HomeController extends AbstractController
 {
@@ -13,8 +16,23 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        // link with home/index sentences 1 capitalize
+        if (!isset($_POST['sentence1'])) {
+            $_POST['sentence1'] = "";
+        }
+
+        // link with home/index sentences 2 spaces to dashes
+        if (!isset($_POST['sentence2'])) {
+            $_POST['sentence2'] = "";
+        }
+
+        // link with Classes (otherwise error "app\Entity classes" is never used
+        $capitalization = new Capitalize();
+        $dashes = new SpacesToDashes();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'controller_name' => 'Dependency Injection',
+            'sentence1' => $capitalization->transform($_POST['sentence1']),
+            'sentence2' => $dashes->transform($_POST['sentence2']),
         ]);
     }
 }
